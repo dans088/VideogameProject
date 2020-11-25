c057729a33655daf5e31c7bef505b6a2128eaff0
razielmtz <razielmartinezcastillo@gmail.com>
2020-11-25 10:37:50 -0600

    Probando colisiones

--- TimeATime.js
+++ TimeATime.js
@@ -1,8 +1,11 @@
 
 //Movement keys (Controls for the game)
 let keysDown = {
-    "a":false,
-    "d":false,
+    "KeyA":false,
+    "KeyD":false,
+    "KeyG":false,
+    "KeyH":false,
+    "KeyM":false,
     "ArrowLeft":false,
     "ArrowRight":false,
     "Space":false
@@ -47,6 +50,7 @@ class Player
 
         });
     }
+    
 
     /*async load3dModel(objModelUrl, mtlModelUrl)
     {
@@ -97,9 +101,9 @@ class Player
     //The player character is updated according to the keys that the player is pressing
     update(){
         // console.log(world.contacts)
-        if(keysDown["a"] || keysDown["ArrowLeft"])
+        if(keysDown["KeyA"] || keysDown["ArrowLeft"])
             this.moveLeft();
-        if(keysDown["d"] || keysDown["ArrowRight"])
+        if(keysDown["KeyD"] || keysDown["ArrowRight"])
             this.moveRight();
         if(keysDown["Space"]){
             //If the player is not currently jumping
@@ -131,6 +135,11 @@ sphereShape = null;
 physicsMaterial = null;
 testPortal = null;
 testPortalBody = null;
+testGround = null;
+testGroundBody = null;
+//Test moving cube
+testCube = null;
+testCubeBody = null;
 
 testmaterials = {
     shadow: new THREE.MeshBasicMaterial({
@@ -200,16 +209,18 @@ function animate()
         // console.log(contact);
         // console.log("BI", contact.bi.id)
         // console.log("BJ", contact.bj.id)
-        if(contact.bi.id == 3 ){
-            player.body.position.set(17,1,0)
-        }
-        // } else if (contact.bi.id == 1){
+        // if(contact.bi.id == 2 ){
+        //     player.body.position.set(17,1,0)
+        // }
+        // if (contact.bi.id == 1){
         //     console.log("Bi: 11111111111111111111")
-        // } else if (contact.bi.id == 3){
-        //     console.log("Bi: 3333333333333333333333333")
+        // } else if (contact.bi.id == 2){
+        //     console.log("Bi: 222222222222222222")
         // } else if (contact.bi.id == 0){
         //     console.log("Bi: 000000000000000000")
-        // }
+        // } else if (contact.bi.id == 3){
+        //     console.log("Bi: 333333333333333")
+        // } 
         // contact.bi.mesh.material = this.materials.shadow;
         // contact.bj.mesh.material = this.materials.colliding;
         
@@ -229,12 +240,15 @@ function run() {
 
     //Update the player character
     player.update();
+    testCube.update();
     
     if(player.playerObject != null){
         //The position of the player character needs to be the same as the position of their cannon body
         player.playerObject.position.copy(playerBody.position);
+        //The position of the portal character needs to be the same as the position of their cannon body
+        testGround.position.copy(testGroundBody.position);
         //The position of the player character needs to be the same as the position of their cannon body
-        testPortal.position.copy(testPortalBody.position);
+        testCube.mesh.position.copy(testCubeBody.position);
     }
 
     // Spin
@@ -276,6 +290,7 @@ async function scene_setup(canvas)
 
     //Create the player character
     await load_ghost();
+    await load_cube();
 
     //Create a pivot and add it to the mesh of the player
     pivot = new THREE.Object3D;
@@ -286,24 +301,41 @@ async function scene_setup(canvas)
     /* Cannon test */
 
     // Create a plane for the floor
-    let PlaneGeometry = new THREE.PlaneGeometry(10,10,15,15);
-    let material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-    let ground = new THREE.Mesh(PlaneGeometry, material);
-    ground.rotation.x = -Math.PI / 2;
-    ground.position.set(1,-1,0);
+    // let PlaneGeometry = new THREE.PlaneGeometry(10,10,15,15);
+    // let material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+    // let ground = new THREE.Mesh(PlaneGeometry, material);
+    // ground.rotation.x = -Math.PI / 2;
+    // ground.position.set(1,-1,0);
 
-    let material2 = new THREE.MeshBasicMaterial( {color: 0x888431} );
-    let ground2 = new THREE.Mesh(PlaneGeometry, material2);
-    ground2.rotation.x = -Math.PI / 2;
-    ground2.position.set(15,-1,0);
-    scene.add(ground);
-    scene.add(ground2);
+    // let material2 = new THREE.MeshBasicMaterial( {color: 0x888431} );
+    // let ground2 = new THREE.Mesh(PlaneGeometry, material2);
+    // ground2.rotation.x = -Math.PI / 2;
+    // ground2.position.set(15,-1,0);
+    // scene.add(ground);
+    // scene.add(ground2);
+    
+    //Create planes for the floor 1st Level
+    const groundGeometry1 = new THREE.BoxGeometry(10, 2, 5 );
+    const materialG1 = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    testGround = new THREE.Mesh( groundGeometry1, materialG1 );
+    testGround.position.set(0,-2,0);
 
-    /*
-    Portals = new THREE.mesh();
-    create_portal();
-    scene.add(Portals);
-    */
+    // var halfExtents = new CANNON.Vec3(1,1,1);
+    // var boxShape = new CANNON.Box(halfExtents);
+    // var boxGeometry = new THREE.BoxGeometry(halfExtents.x*2,halfExtents.y*2,halfExtents.z*2);
+    testGroundBody = addPhysicalBody(testGround, {mass: 0}); 
+    scene.add( testGround );
+
+    let geometry = new THREE.SphereGeometry(2, 36, 36);
+    let testSphere = new THREE.Mesh(geometry, materialG1);
+    testSphere.position.set( -7, 0, 0 );
+    testSphere.rotation.x = Math.PI/1.7;
+
+    //Create cannon body
+    addPhysicalBody(testSphere, {mass: 0});
+
+    // world.addBody(portalBody);
+    scene.add(testSphere);
 
 }
 
@@ -341,67 +373,58 @@ function innitCannon(){
     // world.addContactMaterial(physicsContactMaterial);
 
     // Create a plane for the floor
+
     //Create a shape
-    var groundShape = new CANNON.Box(new CANNON.Vec3(5, 1, 1));
-    //Create a cannon body without mass
-    var groundBody = new CANNON.Body({ mass: 0 });
-    //Add the shape to the body
-    groundBody.addShape(groundShape);
-    //Set the position and rotation of the body
-    groundBody.position.set(1, -1, 0);
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
+    // var groundShape = new CANNON.Box(new CANNON.Vec3(5, 1, 1));
+    // //Create a cannon body without mass
+    // var groundBody = new CANNON.Body({ mass: 0 });
+    // //Add the shape to the body
+    // groundBody.addShape(groundShape);
+    // //Set the position and rotation of the body
+    // groundBody.position.set(1, -1, 0);
+    // groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
 
 
     //Create a shape of second ground
-    var ground2Shape = new CANNON.Box(new CANNON.Vec3(5, 1, 1));
-    //Create a cannon body without mass
-    var ground2Body = new CANNON.Body({ mass: 0 });
-    //Add the shape to the body
-    ground2Body.addShape(ground2Shape);
-    //Set the position and rotation of the body
-    ground2Body.position.set(15,-1,0);
-    ground2Body.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
-    // groundBody.addEventListener("collide",function(e){
-
-    //     console.log("COOOOLLISSION", e);
+    // var ground2Shape = new CANNON.Box(new CANNON.Vec3(5, 1, 1));
+    // //Create a cannon body without mass
+    // var ground2Body = new CANNON.Body({ mass: 0 });
+    // //Add the shape to the body
+    // ground2Body.addShape(ground2Shape);
+    // //Set the position and rotation of the body
+    // ground2Body.position.set(15,-1,0);
+    // ground2Body.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2);
         
-    //     // if(e.body.id == 0){
-    //     //     console.log("The sphere just collided with the ground!");
-    //     //     player.canJump = true;
-    //     // }
-    // });
     //Add it to the world
-    world.addBody(groundBody);
-    world.addBody(ground2Body);
+    // world.addBody(groundBody);
+    // world.addBody(ground2Body);
 }
 
 
 function load_ghost()
-{
-    /*
-    let box_geometry = new THREE.BoxGeometry(1, 1, 1);
-    let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    
-    let playerMesh = new THREE.Mesh(box_geometry, material);
-    */
-    
+{ 
     root = new THREE.Object3D;
 
     //TEST
     //Create cannon body
-    var halfExtents = new CANNON.Vec3(1,0,0);
-    var boxShape = new CANNON.Box(halfExtents);
-    playerBody = new CANNON.Body({ mass: 5 });
+    // var halfExtents = new CANNON.Vec3(1,0,0);
+    // var boxShape = new CANNON.Box(halfExtents);
+    var boxShape = new CANNON.Box(new CANNON.Vec3(0.4 , 0.4, 0));
+    playerBody = new CANNON.Body({ mass: 2 });
     playerBody.addShape(boxShape);
 
-    playerBody.position.set( 0, 0, 0 );
+    playerBody.position.set( -3, 7  , 0 );
 
+    // playerBody.collisionResponse = true;
+    
 
+    body2mesh(playerBody, true);
     //Create player object
     player = new Player(playerBody, root);
 
-   
-
+    // body2mesh(playerBody, true);
+    // player.playerObject.position.copy(playerBody.position);
+    player.body.computeAABB();
     world.addBody(player.body);
 
     //load ghost object
@@ -441,6 +464,8 @@ function create_portal()
     uniforms.noiseTexture.value.wrapS = uniforms.noiseTexture.value.wrapT = THREE.RepeatWrapping;
     uniforms.glowTexture.value.wrapS = uniforms.glowTexture.value.wrapT = THREE.RepeatWrapping;
 
+
+    //Create Portal mesh
     let material = new THREE.ShaderMaterial({
         uniforms: uniforms,
         vertexShader: document.getElementById( 'vertexShader' ).textContent,
@@ -449,35 +474,17 @@ function create_portal()
     } );
     
     let geometry = new THREE.SphereGeometry(2, 36, 36);
-
-    //Create cannon body
-    // var halfExtents = new CANNON.Vec3( 0, 1, 0 );
-    // var boxShape = new CANNON.Box(halfExtents);
-    // portalBody = new CANNON.Body({ mass: 5 });
-    // portalBody.addShape(boxShape);
-
-    // portalBody.position.set( 3, 3, 0 );
-
     let portal = new THREE.Mesh(geometry, material);
     portal.position.set( -3, 3, 0 );
     portal.rotation.x = Math.PI/1.7;
+
+    //Create cannon body
     addPhysicalBody(portal, {mass: 0});
 
     // world.addBody(portalBody);
     scene.add(portal);
 
     testPortal = portal;
-    // testPortalBody = portalBody;
-    
-    // portalBody.addEventListener("collide",function(e){
-
-    //     console.log("PORTAL COLLISION", e);
-        
-    //     // if(e.body.id == 0){
-    //     //     console.log("The sphere just collided with the ground!", e);
-    //     //     player.canJump = true;
-    //     // }
-    // });
 
 }
 
@@ -498,22 +505,266 @@ addPhysicalBody = function (mesh, bodyOptions) {
             (box.max.y - box.min.y) / 2,
             (box.max.z - box.min.z) / 2
         ));
+        console.log("THE SHAPE", shape)
     }
 
     var body = new CANNON.Body(bodyOptions);
-    testPortalBody = body;
     body.addShape(shape);
     body.position.copy(mesh.position);
     body.computeAABB();
     // disable collision response so objects don't move when they collide
     // against each other
-    body.collisionResponse = false;
+    body.collisionResponse = true;
     // keep a reference to the mesh so we can update its properties later
     body.mesh = mesh;
 
-    // body.name = "Cuerpo fisico";
+    world.addBody(body);
+    
+    console.log(body);
 
-    this.world.addBody(body);
-
+    // body2mesh(body, true)
     return body;
 };
+
+//Class for the character that the player controls
+class Cube
+{
+    constructor(mesh, body, speed = 0.1)
+    {
+        this.mesh = mesh;
+        this.body = body;
+        this.speed = speed;
+        this.canJump = true;
+    }
+
+    async load_model(objModeUrl)
+    {
+    
+    }
+
+    //Move position of the player to the left
+    moveLeft()
+    {
+        this.body.position.x -= this.speed;
+    }
+
+    //Move position of the player to the right
+    moveRight()
+    {
+        this.body.position.x += this.speed;
+    }
+
+    //Add velocity in 'y' to the player so they jump
+    jump(){
+        this.body.velocity.y += 20;
+    }
+
+    //The player character is updated according to the keys that the player is pressing
+    update(){
+        if(keysDown["KeyG"]){
+            console.log("G")
+            this.moveLeft();
+        }
+        if(keysDown["KeyH"])
+            this.moveRight();
+        if(keysDown["KeyM"]){
+            //If the player is not currently jumping
+            if(this.canJump){
+                this.jump();
+                this.canJump = false;
+            }
+        }
+    }
+}
+
+function load_cube()
+{
+    let box_geometry = new THREE.BoxGeometry(1, 1, 1);
+    let material = new THREE.MeshBasicMaterial( {color: 0x00fff0} );
+    
+    let cubeMesh = new THREE.Mesh(box_geometry, material);
+
+
+    //TEST
+    //Create cannon body
+    // var halfExtents = new CANNON.Vec3(0,0,0);
+    // var boxShape = new CANNON.Box(halfExtents);
+    // testCubeBody = new CANNON.Body({ mass: 5 });
+    // testCubeBody.addShape(boxShape);
+
+    //Create player object
+    testCubeBody = addPhysicalBody(cubeMesh, {mass: 5})
+    testCube = new Cube(cubeMesh, testCubeBody, 0.1);
+
+    testCubeBody.position.set( 0, 1, 0 );
+    cubeMesh.position.set( 0, 1, 0 );
+
+    // world.addBody(testCube.body);
+    scene.add(testCube.mesh);
+
+    testCube.body.addEventListener("collide",function(e){
+        
+        if(e.body.id == 0){
+            testCube.canJump = true;
+        }
+    });
+
+   
+}
+
+let uno = [0, 6, 12]
+let lai = 0;
+
+function body2mesh(body, wireframe) {
+    console.log("CALCULANDO")
+    var wireframe = wireframe || true;
+    var obj = new THREE.Object3D();
+  
+    for (var l = 0; l < body.shapes.length; l++) {
+      var shape = body.shapes[l];
+  
+      var mesh;
+  
+      switch(shape.type){
+  
+      case CANNON.Shape.types.SPHERE:
+        var sphere_geometry = new THREE.SphereGeometry( shape.radius, 8, 8);
+        mesh = new THREE.Mesh( sphere_geometry, this.currentMaterial );
+        break;
+  
+      case CANNON.Shape.types.PARTICLE:
+        mesh = new THREE.Mesh( this.particleGeo, this.particleMaterial );
+        var s = this.settings;
+        mesh.scale.set(s.particleSize,s.particleSize,s.particleSize);
+        break;
+  
+      case CANNON.Shape.types.PLANE:
+        var geometry = new THREE.PlaneGeometry(10, 10, 4, 4);
+        mesh = new THREE.Object3D();
+        var submesh = new THREE.Object3D();
+        var ground = new THREE.Mesh( geometry, this.currentMaterial );
+        ground.scale.set(100, 100, 100);
+        submesh.add(ground);
+  
+        ground.castShadow = true;
+        ground.receiveShadow = true;
+  
+        mesh.add(submesh);
+        break;
+  
+      case CANNON.Shape.types.BOX:
+        var box_geometry = new THREE.BoxGeometry(  shape.halfExtents.x*2,
+                              shape.halfExtents.y*2,
+                              shape.halfExtents.z*2 );
+        mesh = new THREE.Mesh( box_geometry, this.currentMaterial );
+        break;
+  
+      case CANNON.Shape.types.CONVEXPOLYHEDRON:
+        var geo = new THREE.Geometry();
+  
+        // Add vertices
+        for (var i = 0; i < shape.vertices.length; i++) {
+          var v = shape.vertices[i];
+          geo.vertices.push(new THREE.Vector3(v.x, v.y, v.z));
+        }
+  
+        for(var i=0; i < shape.faces.length; i++){
+          var face = shape.faces[i];
+  
+          // add triangles
+          var a = face[0];
+          for (var j = 1; j < face.length - 1; j++) {
+            var b = face[j];
+            var c = face[j + 1];
+            geo.faces.push(new THREE.Face3(a, b, c));
+          }
+        }
+        geo.computeBoundingSphere();
+        geo.computeFaceNormals();
+        mesh = new THREE.Mesh( geo, this.currentMaterial );
+        break;
+  
+      case CANNON.Shape.types.HEIGHTFIELD:
+        var geometry = new THREE.Geometry();
+  
+        var v0 = new CANNON.Vec3();
+        var v1 = new CANNON.Vec3();
+        var v2 = new CANNON.Vec3();
+        for (var xi = 0; xi < shape.data.length - 1; xi++) {
+          for (var yi = 0; yi < shape.data[xi].length - 1; yi++) {
+            for (var k = 0; k < 2; k++) {
+              shape.getConvexTrianglePillar(xi, yi, k===0);
+              v0.copy(shape.pillarConvex.vertices[0]);
+              v1.copy(shape.pillarConvex.vertices[1]);
+              v2.copy(shape.pillarConvex.vertices[2]);
+              v0.vadd(shape.pillarOffset, v0);
+              v1.vadd(shape.pillarOffset, v1);
+              v2.vadd(shape.pillarOffset, v2);
+              geometry.vertices.push(
+                new THREE.Vector3(v0.x, v0.y, v0.z),
+                new THREE.Vector3(v1.x, v1.y, v1.z),
+                new THREE.Vector3(v2.x, v2.y, v2.z)
+              );
+              var i = geometry.vertices.length - 3;
+              geometry.faces.push(new THREE.Face3(i, i+1, i+2));
+            }
+          }
+        }
+        geometry.computeBoundingSphere();
+        geometry.computeFaceNormals();
+        mesh = new THREE.Mesh(geometry, this.currentMaterial);
+        break;
+  
+      case CANNON.Shape.types.TRIMESH:
+        var geometry = new THREE.Geometry();
+  
+        var v0 = new CANNON.Vec3();
+        var v1 = new CANNON.Vec3();
+        var v2 = new CANNON.Vec3();
+        for (var i = 0; i < shape.indices.length / 3; i++) {
+          shape.getTriangleVertices(i, v0, v1, v2);
+          geometry.vertices.push(
+            new THREE.Vector3(v0.x, v0.y, v0.z),
+            new THREE.Vector3(v1.x, v1.y, v1.z),
+            new THREE.Vector3(v2.x, v2.y, v2.z)
+          );
+          var j = geometry.vertices.length - 3;
+          geometry.faces.push(new THREE.Face3(j, j+1, j+2));
+        }
+        geometry.computeBoundingSphere();
+        geometry.computeFaceNormals();
+        mesh = new THREE.Mesh(geometry, this.currentMaterial);
+        break;
+  
+      default:
+        throw "Visual type not recognized: "+shape.type;
+      }
+  
+      mesh.receiveShadow = true;
+      mesh.castShadow = true;
+      if(mesh.children){
+        for(var i=0; i<mesh.children.length; i++){
+          mesh.children[i].castShadow = true;
+          mesh.children[i].receiveShadow = true;
+          if(mesh.children[i]){
+            for(var j=0; j<mesh.children[i].length; j++){
+              mesh.children[i].children[j].castShadow = true;
+              mesh.children[i].children[j].receiveShadow = true;
+            }
+          }
+        }
+      }
+  
+      var o = body.shapeOffsets[l];
+      var q = body.shapeOrientations[l];
+      mesh.position.set(3, 0  , 0);
+      mesh.quaternion.set(q.x, q.y, q.z, q.w);
+  
+      obj.add(mesh);
+    }
+
+    lai++;
+    scene.add(obj)
+  
+    return obj;
+   };
